69da753af23fbd6088265eb4c4d318ec596449ab
Milena Cárdenas Álzate - Ceiba Software <milena.cardenas@ceiba.com.co>
2018-07-05 14:52:58 -0500

    Adding changed in gradle file

---
+++ src/main/java/co/com/ceiba/Main.java
@@ -0,0 +1,11 @@
+package co.com.ceiba;
+
+import org.springframework.boot.SpringApplication;
+
+public class Main {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Main.class, args);
+	}
+
+}
