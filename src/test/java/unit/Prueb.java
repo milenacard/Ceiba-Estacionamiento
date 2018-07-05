d4c9194c9c84daac6f0eac75fbd9b2741b43f2c6
Milena Cárdenas Álzate - Ceiba Software <milena.cardenas@ceiba.com.co>
2018-07-05 15:00:20 -0500

    test file

---
+++ src/test/java/unit/Prueb.java
@@ -0,0 +1,14 @@
+package unit;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class Prueb {
+
+	@Test
+	public void estoEsUnaPrueba() {
+		assertTrue(1==1);
+	}
+	
+}
