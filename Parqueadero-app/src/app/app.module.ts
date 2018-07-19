ffc7fe39861b3af22e5c551aa014f7768bf96c37
Milena Cárdenas Álzate - Ceiba Software <milena.cardenas@ceiba.com.co>
2018-07-19 16:18:40 -0500

    creating components in the frontend

--- Parqueadero-app/src/app/app.module.ts
+++ Parqueadero-app/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './register/register.component';
+import { ListComponent } from './list/list.component';
+import { PaymentComponent } from './payment/payment.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    ListComponent,
+    PaymentComponent
   ],
   imports: [
     BrowserModule,
