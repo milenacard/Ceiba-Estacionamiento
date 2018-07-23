be3581c6f26ba2ae611cd3dffaf0e5aa13568ed6
Milena Cárdenas Álzate - Ceiba Software <milena.cardenas@ceiba.com.co>
2018-07-23 18:13:46 -0500

    HTML of create-register.component

--- Parqueadero-app/src/app/components/create-register/create-register.component.ts
+++ Parqueadero-app/src/app/components/create-register/create-register.component.ts
@@ -1,3 +1,4 @@
+import { RegistroEntity } from './../../model/registroEntity.model';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,7 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateRegisterComponent implements OnInit {
 
-  constructor() { }
+  private register: RegistroEntity;
+
+  constructor() {
+    this.register = new RegistroEntity();
+  }
 
   ngOnInit() {
   }
